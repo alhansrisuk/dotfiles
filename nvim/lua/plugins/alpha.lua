0feaaa0efcfbcb2c75fee8e6a640fd22bdcdf8fc
albert <alhansrisuk@gmail.com>
2024-11-18 16:51:52 -0500

    switch to alpha dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,7 +1,6 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = { "echasnovski/mini.icons" },
-	config = function()
-		require("alpha").setup(require("alpha.themes.startify").config)
-	end,
+  "goolord/alpha-nvim",
+  config = function()
+    require("alpha").setup(require("alpha.themes.dashboard").config)
+  end,
 }
