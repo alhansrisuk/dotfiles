c2190ed6c5f61d4998a079848d4a8c27a3694b03
albert <alhansrisuk@gmail.com>
2024-11-28 07:18:38 -0500

    add aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias p='emacs ~/storage/shared/org/personal.org'
+    alias j='emacs ~/storage/shared/org/journal.org'
 end
