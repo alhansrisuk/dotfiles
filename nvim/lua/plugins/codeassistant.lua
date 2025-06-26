8879c3e45e432e5d8d64fa8ba00a832b0dbebbb6
Albert Hansrisuk <albert.hansrisuk@voxmedia.com>
2025-06-26 10:51:48 -0400

    use gemini for codecompanion

--- nvim/lua/plugins/codeassistant.lua
+++ nvim/lua/plugins/codeassistant.lua
@@ -1,8 +1,30 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = true,
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	-- config = true,
+	opts = {
+		strategies = {
+			chat = {
+				adapter = "gemini",
+			},
+			inline = {
+				adapter = "gemini",
+			},
+			cmd = {
+				adapter = "gemini",
+			},
+		},
+		adapters = {
+			gemini = function()
+				return require("codecompanion.adapters").extend("gemini", {
+					env = {
+						api_key = "",
+					},
+				})
+			end,
+		},
+	},
 }
